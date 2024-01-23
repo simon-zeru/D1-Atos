7e660e0a05cf31becbb48449068d7cb86bc55234
Simon <simzeru@gmail.com>
2024-01-23 15:15:29 +0100

    allègement du js

--- src/js/accueil.js
+++ src/js/accueil.js
@@ -29,36 +29,6 @@ buttonSupercalc.addEventListener('click', () => changeTab(4));
     }
 }
 
-// Partie animation de la section clients
-
-// Partie animation des chevrons du carrousel clients
-
-function updateChevronClasses() {
-    var windowWidth = window.innerWidth;
-    var chevronStart = document.querySelector('.start');
-    var chevronEnd = document.querySelector('.end');
-
-    if (windowWidth < 992) {
-        // Si fenêtre format d'ordinateur alors remplacer les chevrons left et right 
-        chevronStart.src = 'icons/chevron-up.svg';
-        chevronEnd.src = 'icons/chevron-down.svg';
-
-    } else {
-        // Sinon faire l'inverse
-        chevronStart.src = 'icons/chevron-left.svg';
-        chevronEnd.src = 'icons/chevron-right.svg';
-    }
-}
-
-// Appelez la fonction initiale pour définir les classes en fonction de la taille initiale
-updateChevronClasses();
-
-// Ajoutez un écouteur d'événements pour le redimensionnement de la fenêtre
-window.addEventListener('resize', function () {
-    // Appelez la fonction lorsqu'il y a un changement de taille
-    updateChevronClasses();
-});
-
 // Partie animation du carrousel clients
 
 const slider = document.body.querySelector('#clients');
